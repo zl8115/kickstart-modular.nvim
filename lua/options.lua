6e52faa8dd19540f441f3355b3302ae78fd69bc0
zl8115 <ze.rong96@gmail.com>
2025-02-24 17:18:18 +0000

    [BASE] Initial config mods

--- lua/options.lua
+++ lua/options.lua
@@ -66,4 +66,9 @@ vim.opt.scrolloff = 10
 -- See `:help 'confirm'`
 vim.opt.confirm = true
 
+-- ShiftWidth and TabStop
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = false
+
 -- vim: ts=2 sts=2 sw=2 et
