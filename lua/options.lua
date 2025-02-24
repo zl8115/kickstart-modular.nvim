6e2a38066e73bdff63dbef9e282e98f313df0ad9
zl8115 <ze.rong96@gmail.com>
2025-02-24 17:18:18 +0000

    [BASE] Initial config mods

--- lua/options.lua
+++ lua/options.lua
@@ -62,4 +62,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- ShiftWidth and TabStop
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = false
+
 -- vim: ts=2 sts=2 sw=2 et
