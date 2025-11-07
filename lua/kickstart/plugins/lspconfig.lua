41555e2c14d73e13e32646a157e3357e2d62e5e8
zl8115 <ze.rong96@gmail.com>
2025-11-07 17:32:55 +0000

    [BASE] Add cmake lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -123,6 +123,7 @@ return {
       ---@type table<string, vim.lsp.Config>
       local servers = {
         clangd = {},
+        cmake = {},
         -- gopls = {},
         pyright = {},
         -- rust_analyzer = {},
