d13e57556097ee063b4565b54aa12fc7211cbcb4
zl8115 <ze.rong96@gmail.com>
2026-03-18 23:41:23 +0000

    [BASE] Add Obsidian

---
+++ lua/custom/plugins/obsidian.lua
@@ -0,0 +1,79 @@
+return {
+  'obsidian-nvim/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    'nvim-lua/plenary.nvim',
+
+    -- see above for full list of optional dependencies ☝️
+  },
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    workspaces = {
+      {
+        name = 'personal',
+        path = vim.g.obsidian_vault,
+      },
+    },
+
+    frontmatter = {
+      enabled = false,
+    },
+
+    templates = {
+      folder = '_templates',
+      date_format = '%Y-%m-%d',
+      time_format = '%H:%M',
+    },
+
+    completion = {
+      min_chars = 2,
+      create_new = false,
+    },
+
+    daily_notes = {
+      folder = 'Daily',
+      workdays_only = false,
+    },
+
+    footer = {
+      enabled = false,
+    },
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
+      name = 'telescope.nvim',
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      note_mappings = {
+        -- Create a new note from your query.
+        new = '<C-x>',
+        -- Insert a link to the selected note.
+        insert_link = '<C-l>',
+      },
+      tag_mappings = {
+        -- Add tag(s) to current note.
+        tag_note = '<C-x>',
+        -- Insert a tag at the current location.
+        insert_tag = '<C-l>',
+      },
+    },
+
+    ui = {
+      ignore_conceal_warn = true,
+    },
+
+    legacy_commands = false,
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
